2a8918ccf82a9a31956b430a4ae15b15870e8d41
vdorchan <vdorchan@gmail.com>
2018-08-15 18:11:57 +0800

    feat:turn the help doc to json file

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const yargs = require('yargs')
 
 const server = require('./lib/server')
 
+const pkg = require('./package.json')
+const { turnHelpDoc } = require('./lib/util')
+
 const argv = yargs
   .usage('$0 [options] <source>')
   .options({
@@ -16,11 +19,25 @@ const argv = yargs
     delay: {
       alias: 'd',
       description: 'Add delay to responses (ms)'
+    },
+    turnDoc: {
+      alias: 't',
+      description: '传入开发文档的链接和json文件名（可选，默认以第一级目录名），自动将其转换为json文件'
     }
   })
   .help('help')
   .alias('help', 'h')
   .version(pkg.version)
+  .epilog('https://github.com/vdorchan/pc-mock')
   .alias('version', 'v').argv
 
-server(argv)
+const { turnDoc, source } = argv
+
+if (turnDoc) { 
+  turnHelpDoc(turnDoc, argv._[0]).then(file => {
+    console.log('成功将目标文档转换为json文件，文件地址在：')
+    console.log(file)
+  })
+} else {
+  server(argv)
+}
